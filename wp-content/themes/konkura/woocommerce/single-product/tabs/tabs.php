204417214bcaed9ce86a0b1b7db7196d65d1f053
Lázaro Reyes <lazaro.reyesfierro@gmail.com>
2015-05-05 12:24:47 -0600

    se arreglan las tabs de single product

--- wp-content/themes/konkura/woocommerce/single-product/tabs/tabs.php
+++ wp-content/themes/konkura/woocommerce/single-product/tabs/tabs.php
@@ -21,23 +21,25 @@ $tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
 if ( ! empty( $tabs ) ) : ?>
 
-	<div class="woocommerce-tabs">
-		<ul class="tabs nav nav-tabs">
-			<?php foreach ( $tabs as $key => $tab ) : ?>
+    <div class="col-sm-12 margin-top">
+        <div class="woocommerce-tabs">
+            <ul class="tabs nav nav-tabs">
+                <?php foreach ( $tabs as $key => $tab ) : ?>
 
-				<li class="<?php echo $key ?>_tab">
-					<a href="#tab-<?php echo $key ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', $tab['title'], $key ) ?></a>
-				</li>
+                    <li class="<?php echo $key ?>_tab">
+                        <a href="#tab-<?php echo $key ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', $tab['title'], $key ) ?></a>
+                    </li>
 
-			<?php endforeach; ?>
-		</ul>
-		<?php foreach ( $tabs as $key => $tab ) : ?>
+                <?php endforeach; ?>
+            </ul>
+            <?php foreach ( $tabs as $key => $tab ) : ?>
 
-			<div class="panel entry-content" id="tab-<?php echo $key ?>">
-				<?php call_user_func( $tab['callback'], $key, $tab ) ?>
-			</div>
+                <div class="panel entry-content" id="tab-<?php echo $key ?>">
+                    <?php call_user_func( $tab['callback'], $key, $tab ) ?>
+                </div>
 
-		<?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
 	</div>
 
 <?php endif; ?>
